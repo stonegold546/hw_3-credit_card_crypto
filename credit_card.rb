7a812a7b09e4068cc06c4650928e46a4af349aa6
James Uanhoro <stonegold546@gmail.com>
2015-03-25 19:44:51 +0800

    Done Assignment A

--- credit_card.rb
+++ credit_card.rb
@@ -3,19 +3,25 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above (do not forget the '@')
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
-     }.to_json
+      number: @number, expiration_date: @expiration_date, owner: @owner, credit_number: @credit_number
+    }.to_json
   end
 
   # returns all card information as single string
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,9 +3,24 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
+  def my_inject(a, b)
+    a.each_index.inject(0) { |sum, index|
+      if index % 2 == 0
+        sum + a[index]
+      elsif b * a[index] >= 10
+        sum + my_inject(((b * a[index]).to_s.chars.map(&:to_i)), 1)
+      else
+        sum + (b * a[index])
+      end
+    }
+  end
+  
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_a.reverse!
+    checksum = my_inject(nums_a, 2)
+    checksum % 10 == 0
   end
 end
