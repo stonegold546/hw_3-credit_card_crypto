42f49b2c4bad678e6bc835fa865617dbe04011df
James Uanhoro <stonegold546@gmail.com>
2015-04-02 04:23:08 +0800

    Done assignment B, I think

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,7 +7,7 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caeser cipher
-      document.to_s.chars.map{ |x| x.ord + key }
+      document.to_s.chars.map { |x| x.ord + key }
     end
 
     # Decrypts String document using integer key
@@ -17,7 +17,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caeser cipher
-      document.map{ |x| (x.ord - key).chr }.join
+      document.map { |x| (x.ord - key).chr }.join
     end
   end
 
@@ -27,22 +27,23 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
-    
+
     def self.fill_book(k, r, c)
-      begin
+      loop do
         a = r.rand(128)
         c[k] = a unless c.value?(a)
-      end until c.key?(k)
+        break if c.key?(k)
+      end
     end
-    
+
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
-      code_book = Hash.new()
+      code_book = {}
       rand1 = Random.new(key)
       (32..127).each do |x|
         fill_book(x, rand1, code_book)
       end
-      document.to_s.chars.map{ |x| code_book[x.ord] }
+      document.to_s.chars.map { |x| code_book[x.ord] }
     end
 
     # Decrypts String document using integer key
@@ -52,12 +53,12 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
-      code_book = Hash.new()
+      code_book = {}
       rand1 = Random.new(key)
       (32..127).each do |x|
         fill_book(x, rand1, code_book)
       end
-      document.map{ |x| (code_book.key(x)).chr }.join
+      document.map { |x| (code_book.key(x)).chr }.join
     end
   end
 end
