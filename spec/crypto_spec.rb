917731df30de2c081cb0581d05fccf52e5f9ba4a
James Uanhoro <stonegold546@gmail.com>
2015-04-09 19:31:06 +0800

    Removed long line

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -39,7 +39,8 @@ describe 'Test card info encryption' do
   # TODO: Add tests for double transposition and AES ciphers
   #       Can you DRY out the tests using metaprogramming? (see lecture slide)
   methods = [
-    ['Double Transposition Cipher', DoubleTranspositionCipher], ['AES Cipher', AesCipher]
+    ['Double Transposition Cipher', DoubleTranspositionCipher],
+    ['AES Cipher', AesCipher]
   ]
   methods.each do |name, meth|
     describe "Using #{name} cipher" do
